6fda8834143dc651830a755a726a1a079610836a
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-07 18:39:55 +0200

    Axios, UseEffect - FetchProducts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import { Product } from "./components/Product";
 import { products } from "./data/products";
 import "./App.scss";
+import axios from "axios";
+import { IProduct } from "./models";
+import { useEffect } from "react";
 
 function App() {
+  const fetchProducts = async () => {
+    const response = await axios.get<IProduct[]>(
+      "https://fakestoreapi.com/products?limit=10"
+    );
+    console.log(response);
+    
+  };
+
+  useEffect(() => {
+
+fetchProducts()
+  }, []);
+
   return (
     <div className="App">
       <div>Product</div>
@@ -14,4 +30,3 @@ function App() {
 }
 
 export default App;
- 
